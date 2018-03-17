c13d79028ca924aaf26ef078ed5ad6b6da21ba40
Kevin McCarthy <kevinpatrickmccarthy@gmail.com>
2018-03-17 19:00:46 +0000

    able to call function from within function
    - added FunctionCall node
    - generator can deal with value or function call in body
    - either function can take n arguments

--- compiler.rb
+++ compiler.rb
@@ -66,9 +66,21 @@ class Parser
     consume(:def)
     identifier = consume(:identifier)
     arg_names = parse_arg_names
-    return_value = consume_body
+    body = parse_expression
     consume(:end)
-    DefNode.new(identifier.value, arg_names, return_value.value)
+    DefNode.new(identifier.value, arg_names, body)
+  end
+
+  def parse_expression
+    if check_next_token_type(:integer) 
+      @tokens.shift
+    elsif check_next_token_type(:identifier) && @tokens[1].type == :open_paren
+       parse_function_call
+    elsif check_next_token_type(:identifier) 
+       @tokens.shift
+    else
+      raise RuntimeError.new("expected integer or identifier but got #{@tokens[0].type}")
+    end
   end
 
   def parse_arg_names
@@ -88,16 +100,35 @@ class Parser
     args
   end
 
-  def check_next_token_type(expected_type)
-    @tokens[0].type == (expected_type)
+  def parse_arg_expressions
+    consume(:open_paren)
+    args = []
+    args = addAnyArgExpressions(args)
+    consume(:close_paren)
+
+    args
   end
 
-  def consume_body
-    if @tokens[0].type == :integer || @tokens[0].type == :identifier
-      @tokens.shift
-    else
-      raise RuntimeError.new("expected #{expected_type} but got #{@tokens[0].type}")
+  def addAnyArgExpressions(args)
+    return if check_next_token_type(:close_paren)
+    args << parse_expression
+    if check_next_token_type(:separator)
+      consume(:separator)
+      addAnyArgExpressions(args)
     end
+    args
+  end
+
+  def check_next_token_type(expected_type)
+    @tokens[0].type == expected_type
+  end
+
+ 
+
+  def parse_function_call
+    function_name = @tokens.shift
+    arg_expressions = parse_arg_expressions
+    FunctionCall.new(function_name, arg_expressions)
   end
 
   def consume(expected_type)
@@ -109,8 +140,9 @@ class Parser
   end
 end
 
-DefNode = Struct.new(:name, :arg_names, :return_value)
+DefNode = Struct.new(:name, :arg_names, :body)
 VarAssignmentNode = Struct.new(:var_name, :var_value)
+FunctionCall = Struct.new(:name, :arg_expressions)
 
 class Generator
   def generate(nodes)
@@ -120,17 +152,38 @@ class Generator
           str += "var #{node.var_name} = #{node.var_value};"
         end
         if node.is_a?(DefNode)
-          str += "function #{node.name} (#{args_string(node.arg_names)}) { return #{node.return_value} }"
+          str += "function #{node.name} (#{args_names_string(node.arg_names)}) { return #{body_string(node.body)} }"
         end
     end
     str
   end
 
-  def args_string(arg_names)
+  def body_string(body)
+    if body.is_a?(FunctionCall)
+      body.name.value + '(' + args_expression_string(body.arg_expressions) + ')' if body.name
+    else
+      body.value
+    end
+  end
+
+  def args_names_string(arg_names)
+    return '' if !arg_names
+    return '' if arg_names.empty?
     str = ''
     arg_names.each_with_index do |arg_name, index|
       str += arg_name if index == 0
       str += ", #{arg_name}" if index > 0
+    end
+    str
+  end
+
+  def args_expression_string(arg_expressions)
+    return '' if !arg_expressions
+    return '' if arg_expressions.empty?
+    str = ''
+    arg_expressions.each_with_index do |arg_expression, index|
+      str += arg_expression.value if index == 0
+      str += ", #{arg_expression.value}" if index > 0
     end
     str
   end
@@ -142,4 +195,9 @@ tokens = Tokenizer.new(File.read("test.src")).tokenize
 
 tree = Parser.new(tokens).parse
 #puts tree
-puts Generator.new.generate(tree)
+generated =  Generator.new.generate(tree)
+
+RUNTIME = "function add(x, y) { return x + y };"
+TEST = "console.log(newFunction(17))"
+puts [RUNTIME, generated, TEST].join("\n")
+
